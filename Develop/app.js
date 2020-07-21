680fc779012b6946a640000bd52d7b4e8b7da42e
Monica Rangel <monicarangel@monicas-mbp-3.home>
2020-07-20 18:19:59 -0700

    Manager input created

--- Develop/app.js
+++ Develop/app.js
@@ -9,27 +9,91 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
-
-
+const team = [];
+const idArray = [];
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+function appMenu() {
+    function createManager() {
+        console.log("Build your team");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "manager",
+                message: "Manager. Please enter your name.",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter at least one character.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "Please enter your email.",
+                validate: answer => {
+                    const pass = answer.match(
+                        /\S+@\S+\.\S+/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a valid email address.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "Please enter your ID.",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+            },
+            {
+                type: "input",
+                name: "managerPhone",
+                message: "Please enter your phone number.",
+                validate: answer => {
+                    if (answer !== "") {
+                        return true;
+                    }
+                    return "Please enter phone number.";
+                }
+            },
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+            {
+                type: "number",
+                name: "teamSize",
+                message: "Please enter the number of employees you have. (Numbers only)",
+                validate: answer => {
+                    const pass = answer.match(
+                        /^[1-9]\d*$/
+                    );
+                    if (pass) {
+                        return true;
+                    }
+                    return "Please enter a positive number greater than zero.";
+                }
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.manager, answers.managerEmail, answers.managerID, answers.managerPhone, answers.teamSize);
+            team.push(manager);
+            idArray.push(answers.managerId);
+            createTeam();
+        });
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+    }
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    createManager();
+
+}
+
+appMenu();
