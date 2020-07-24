a0cdfb9567235a502ccab48e4deb7681ebc58221
Monica Rangel <monicarangel95@github.com>
2020-07-23 18:21:22 -0700

    function addEngineer to code

--- Develop/app.js
+++ Develop/app.js
@@ -89,7 +89,7 @@ function appMenu() {
             idArray.push(answers.managerId);
             createTeam();
         });
-//inquirer prompts for engineer or intern
+        //inquirer prompts for engineer or intern
         inquirer.prompt([
             {
                 type: "list",
@@ -113,6 +113,70 @@ function appMenu() {
                     buildTeam();
             }
         });
+        function addEngineer() {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "engineerName",
+                    message: "What is your engineer's name?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }
+                        return "Please enter at least one character.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "engineerId",
+                    message: "What is your engineer's id?",
+                    validate: answer => {
+                        const pass = answer.match(
+                            /^[1-9]\d*$/
+                        );
+                        if (pass) {
+                            if (idArray.includes(answer)) {
+                                return "This ID is already taken. Try again";
+                            } else {
+                                return true;
+                            }
+
+                        }
+                        return "Please enter a positive number greater than zero.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "engineerEmail",
+                    message: "What is your engineer's email?",
+                    validate: answer => {
+                        const pass = answer.match(
+                            /\S+@\S+\.\S+/
+                        );
+                        if (pass) {
+                            return true;
+                        }
+                        return "Please enter a valid email address.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "engineerGithub",
+                    message: "What is your engineer's GitHub username?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }
+                        return "Please enter at least one character.";
+                    }
+                }
+            ]).then(answers => {
+                const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+                teamMembers.push(engineer);
+                idArray.push(answers.engineerId);
+                createTeam();
+            });
+        }
 
 
     }
