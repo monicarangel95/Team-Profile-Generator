47212770a564005f7312f9152ce26b331bf5c88d
Monica Rangel <monicarangel95@github.com>
2020-07-23 18:24:23 -0700

    function addIntern

--- Develop/app.js
+++ Develop/app.js
@@ -177,7 +177,70 @@ function appMenu() {
                 createTeam();
             });
         }
+        function addIntern() {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "internName",
+                    message: "What is your intern's name?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }
+                        return "Please enter at least one character.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "internId",
+                    message: "What is your intern's id?",
+                    validate: answer => {
+                        const pass = answer.match(
+                            /^[1-9]\d*$/
+                        );
+                        if (pass) {
+                            if (idArray.includes(answer)) {
+                                return "This ID is already taken. Try again!";
+                            } else {
+                                return true;
+                            }
 
+                        }
+                        return "Please enter a positive number greater than zero.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "internEmail",
+                    message: "What is your intern's email?",
+                    validate: answer => {
+                        const pass = answer.match(
+                            /\S+@\S+\.\S+/
+                        );
+                        if (pass) {
+                            return true;
+                        }
+                        return "Please enter a valid email address.";
+                    }
+                },
+                {
+                    type: "input",
+                    name: "internSchool",
+                    message: "What is your intern's school?",
+                    validate: answer => {
+                        if (answer !== "") {
+                            return true;
+                        }
+                        return "Please enter at least one character.";
+                    }
+                }
+            ]).then(answers => {
+                const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+                teamMembers.push(intern);
+                idArray.push(answers.internId);
+                createTeam();
+            });
+        }
 
     }
 
