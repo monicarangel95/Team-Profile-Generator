cdf8321ea45d19005d23cb2629b837b74f20d994
Monica Rangel <monicarangel95@github.com>
2020-07-23 18:36:27 -0700

    code to define and export the Intern class

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+//Const to require Employee class
+const Employee = require("./Employee");
+class Intern extends Employee {
+  constructor(name, id, email, school) {
+    super(name, id, email);
+    this.school = school;
+  }
+  getRole() {
+    return "Intern";
+  }
+  getSchool() {
+    return this.school;
+  }
+    
+}
+
+//export to allow usage with NPM
+module.exports = Intern;
